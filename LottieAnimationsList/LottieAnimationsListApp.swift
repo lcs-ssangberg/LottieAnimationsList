e3bb233c7600248f0ae6f5ea0c8b1852c0847710
Sebastian Sangberg <ssangberg@lcs.on.ca>
2025-02-07 12:18:17 -0500

    Initial Commit

---
+++ LottieAnimationsList/LottieAnimationsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationsListApp.swift
+//  LottieAnimationsList
+//
+//  Created by Sebastian on 2025-02-07.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
